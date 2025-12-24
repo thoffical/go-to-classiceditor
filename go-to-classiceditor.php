5fe2a8bcc0a733dcd4c648a8905638937e159555
Thirdy <163488855+thoffical@users.noreply.github.com>
2025-12-24 11:44:30 +0800

    Add Go to Classic Editor plugin

---
+++ go-to-classiceditor.php
@@ -0,0 +1,24 @@
+<?php
+/**
+ * Plugin Name: Go to Classic Editor 
+ * Description: A plugin that adds a button in Gutenberg to switch to Classic Editor and back.
+ * Version: 1.0
+ * Author: Thirdy
+ */
+
+// Exit if accessed directly.
+if (!defined('ABSPATH')) {
+    exit;
+}
+
+// Enqueue script to add the button in Gutenberg.
+function gcs_enqueue_block_editor_assets() {
+    wp_enqueue_script(
+        'gcs-switch-editor', // Handle for the script
+        plugin_dir_url(__FILE__) . 'js/switch-editor.js', // URL for the JS file
+        array('wp-blocks', 'wp-element', 'wp-components'), // Dependencies
+        null,
+        true
+    );
+}
+add_action('enqueue_block_editor_assets', 'gcs_enqueue_block_editor_assets');
